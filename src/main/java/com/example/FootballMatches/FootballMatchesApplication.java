b9dc089accfde5106a8dcaf1699a7663c9bcca8d
Tomasz Olszewski <tomaszek.olszewski@gmail.com>
2022-11-29 18:50:46 +0100

    Initial commit

---
+++ src/main/java/com/example/FootballMatches/FootballMatchesApplication.java
@@ -0,0 +1,13 @@
+package com.example.FootballMatches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballMatchesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballMatchesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FootballMatches/FootballMatchesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FootballMatches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballMatchesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
